c207a95dbb279b913bd279bd18c09ca0ba368e61
Jshin720 <jeffshin720@gmail.com>
2021-12-05 18:36:42 -0500

    finished user schema

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
 
-//mongodb+srv://admin:CfT2uwGNibdRePD@mern.desnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+mongoose
+  .connect(db, { useNewUrlParser: true})
+  .then(() => console.log("Conneted to mongoDb"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello a/A!");
 }); 
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 
 const port = process.env.Port || 5000;
 
